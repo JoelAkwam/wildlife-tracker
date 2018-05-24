c966f633dea7452992a1003bff0dbc39e4a93fbe
joelakwam <jakwam@gmail.com>
2018-05-24 15:45:40 +0000

    Add another route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,5 +14,23 @@ public class App{
             model.put("template", "templates/index.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
+
+        get("/homepage", (request, respond) ->{
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/homepage.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        post("/details", (request, respond) ->{
+            Map<String, Object> model = new HashMap<String, Object>();
+            String ranger = request.queryParams("rander");
+            String animal = request.queryParams("animal");
+            String endangered = request.queryParams("endangered");
+            String location = request.queryParams("location");
+            String condition = request.queryParams("condition");
+            String age = request.queryParams("age");
+            model.put("template", "templates/homepage.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
     }
 }
